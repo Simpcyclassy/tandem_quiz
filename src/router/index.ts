c35ed529d029af6c2ebd36c28b2cc4aadfb550d8
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-10-27 16:11:27 +0100

    added landing page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import QuestionCard from '../views/QuestionCard.vue'
+import QuizScore from '../views/QuizScore.vue'
+import Welcome from '../views/Welcome.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Welcome',
+    component: Welcome
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/questions',
+    name: 'QuestionCard',
+    component: QuestionCard
+  },
+  {
+    path: '/score',
+    name: 'QuizScore',
+    component: QuizScore
   }
 ]
 
