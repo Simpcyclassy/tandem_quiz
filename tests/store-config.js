ed0a47916bcda21997b71c6a13de1bb1803b726d
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-11-01 05:04:18 +0100

    test: should emit event when the play again button is clicked

---
+++ tests/store-config.js
@@ -0,0 +1,12 @@
+import mutations from '@/store/mutations'
+import getters from '@/store/getters'
+
+export default {
+  state: {
+    quizSet: {},
+    correctAnswerCount: 0
+  
+  },
+  mutations,
+  getters
+}
